5b0c46f8f2d73da272056e2ba5a99784dc99a31a
hunganhnb2504 <hunganhnbng@gmail.com>
2024-08-20 12:43:28 +0700

    add

--- T2209M_NguyenHungAnh/src/main/java/com/restaurant/t2209m_nguyenhoangphuong/repository/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.restaurant.t2209m_nguyenhoangphuong.repository;
-
-import com.restaurant.t2209m_nguyenhoangphuong.entities.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, Integer> {}
-
