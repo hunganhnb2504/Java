8efc1e6e968e5b318623327d1c2ce160e648d8d0
hunganhnb2504 <117131133+hunganhnb2504@users.noreply.github.com>
2024-08-20 12:38:01 +0700

    Update T2209MNguyenHungAnhApplicationTests.java

--- T2209M_NguyenHungAnh/src/test/java/com/restaurant/t2209m_nguyenhoangphuong/T2209MNguyenHoangPhuongApplicationTests.java
+++ T2209M_NguyenHungAnh/src/test/java/com/restaurant/t2209m_nguyenhoangphuong/T2209MNguyenHoangPhuongApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class T2209MNguyenHoangPhuongApplicationTests {
+class T2209MNguyenHungAnhApplicationTests {
 
     @Test
     void contextLoads() {
